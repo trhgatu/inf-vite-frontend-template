2472f95f946b2b8d7fb65b0388b3f0e2f13e29cc
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-06-23 08:26:37 +0700

    ci(github): setup GitHub Actions workflow for lint & build

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -29,7 +29,7 @@ export const LoginForm = () => {
     if (isAuthenticated) {
       navigate('/')
     }
-  }, [isAuthenticated])
+  }, [isAuthenticated, navigate])
 
   useEffect(() => {
     if (error) {
